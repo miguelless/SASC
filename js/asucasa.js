4402c28354521e63aa69dfdb2fc7406cc7842295
miguelless <miguelcasascordero@gmail.com>
2018-02-03 17:35:16 -0300

    Add files via upload

---
+++ js/asucasa.js
@@ -0,0 +1,21 @@
+// scroll to top button
+// jQuery( document ).ready( function ( $ ) {
+//     $( "#back-to-top" ).hide();
+//     $( function () {
+//         $( window ).scroll( function () {
+//             if ( $( this ).scrollTop() > 100 ) {
+//                 $( '#back-to-top' ).fadeIn();
+//             } else {
+//                 $( '#back-to-top' ).fadeOut();
+//             }
+//         } );
+
+//         // scroll body to 0px on click
+//         $( '#back-to-top a' ).click( function () {
+//             $( 'body,html' ).animate( {
+//                 scrollTop: 0
+//             }, 800 );
+//             return false;
+//         } );
+//     } );
+// } );

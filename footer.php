3ae2ddf3498496caa9bb8cb96990c63640e7a568
miguelless <miguelcasascordero@gmail.com>
2018-02-03 18:28:29 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -20,18 +20,16 @@
 		<div class="col-xs-12 col-md-4 col-md-offset-4">
 			<h5 style="color: #fff"><strong>Para Mayor Informacion Comuniquese a: </strong></h5>
 			<h6 style="color: #fff"><strong>Telefonos y Whatapps</strong></h6>
-			<h7 style="color: #fff; margin-left: 10px;">+56 9 6658 8740</h7><br>
-			<h7 style="color: #fff;margin-left: 10px;">+56 9 6646 7089</h7>
+
 
 			<h6 style="color: #fff"><strong>Email</strong></h6>
-			<h7 style="color: #fff;margin-left: 10px;">info@serviciosasucasa.cl</h7><br>
-			<h7 style="color: #fff;margin-left: 10px;">contacto@serviciosasucasa.cl</h7>
+
 			<br><br>
 		</div>
 
 
 		<div class="span12">
-			<p class="muted" style="color: #fff;text-align: center;"><small><strong>Copyright 2018© www.serviciosasucasa.cl</strong></small></p>
+			<p class="muted" style="color: #fff;text-align: center;"><small><strong>Copyright 2018© </strong></small></p>
 		</div>
 	</div>
 </footer><!-- #colophon -->
